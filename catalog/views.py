784107963b5242d825ea28e0b15210a0016c34f0
Juan <jlizarraga@unomaha.edu>
2021-02-07 12:32:47 -0600

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -138,14 +138,14 @@ from django.urls import reverse_lazy
 
 from catalog.models import Author
 
-
-class AuthorCreate(CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     initial = {'date_of_death': '11/06/2020'}
+    permission_required = 'catalog.can_mark_returned'
 
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = '__all__'  # Not recommended (potential security issue if more fields added)
 

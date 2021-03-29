ce1dd883c7640a27669db45b71fc00da89376424
Juan <jlizarraga@unomaha.edu>
2021-03-28 22:31:31 -0600

    Create unitTest_viewBookAdd.py

---
+++ tests/unitTest_viewBookAdd.py
@@ -0,0 +1,47 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+
+class ll_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Safari()
+
+    def test_ll(self):
+        user = "cjuanito"
+        pwd = "Python2021"
+
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://127.0.0.1:8000/admin")
+        time.sleep(3)
+        elem = driver.find_element_by_id("id_username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_id("id_password")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem = driver.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div/form/div[3]/input").click()
+        time.sleep(3)
+        #assert "Logged in"
+        try:
+            # attempt to find the 'Logout' button - if found, logged in
+           elem = driver.find_element_by_xpath("/html/body/div/div[2]/div/div[1]/div[1]/div[2]/table/tbody/tr[3]/td[1]/a").click()
+
+           assert True
+
+        except NoSuchElementException:
+            self.fail("Login Failed - user may not exist")
+            assert False
+
+        time.sleep(3)
+
+
+def tearDown(self):
+    self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
